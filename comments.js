760373314722efe574c75277ca962583016e29ba
Arif Ramadan <r07.arif@gmail.com>
2023-09-25 13:10:30 +0700

    git commit -m "Copilot third commit"

---
+++ comments.js
@@ -0,0 +1,18 @@
+// Create web server for comments
+// ==============================================
+
+// Import modules
+const express = require('express');
+const router = express.Router();
+const commentController = require('../controllers/commentController');
+
+// handle requests
+router.get('/', commentController.comment_list);
+router.get('/create', commentController.comment_create_get);
+router.post('/create', commentController.comment_create_post);
+router.get('/:id/delete', commentController.comment_delete_get);
+router.post('/:id/delete', commentController.comment_delete_post);
+router.get('/:id/update', commentController.comment_update_get);
+router.post('/:id/update', commentController.comment_update_post);
+router.get('/:id', commentController.comment_detail);
+
